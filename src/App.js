445c4835e7bb22a2fc52e1226b875cf78378ee0a
goro5685 <gorogore060@gmail.com>
2023-12-08 20:51:50 +0300

    this is my changes while building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+    <Nav></Nav>
+    </>
   );
-}
+};
 
 export default App;
