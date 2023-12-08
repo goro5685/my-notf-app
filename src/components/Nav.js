3fd043e598eb7206dcae30fc8cae56544955124d
goro5685 <gorogore060@gmail.com>
2023-12-08 21:10:05 +0300

    hello this another commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from '../images/Logo .svg'
 
 const Nav = () => {
+    const[menuOpen, setmenuOpen] =useState(false);
+
+    const toggleMenu = () =>{
+
+        setmenuOpen(!menuOpen);
+    }
     return(
-        <nav>
-        Nav page
+        <nav className={`navbar ${menuOpen ? "Open" : ""}`}>
+        <a href="/" className="logo">
+        <img src={logo} alt="logo"/>
+        </a>
+        {/* mobile navbar*/}
+        <div className="menu-icon" onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+        {/* nav items*/}
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/">Services</a>
+            </li>
+            <li>
+                <a href="/">Menu</a>
+            </li>
+            <li>
+                <a href="/">Reservations</a>
+            </li>
+            <li>
+                <a href="/">Order Online</a>
+            </li>
+            <li>
+                <a href="/">Login</a>
+            </li>
+        </ul>
+        {/*  */}
+
         </nav>
+
     );
 };
 
